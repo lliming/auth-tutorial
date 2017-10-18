b2e9e085457cbb5ef2018edece4ea22ca0a784df
Lee Liming <lliming@uchicago.edu>
2017-10-18 12:28:21 -0600

    Add files via upload

---
+++ auth_example.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 17 16:44:23 2017
+
+@author: liming
+"""
+
+from flask import Flask, url_for, session, redirect, request
+import globus_sdk
+
+app = Flask(__name__)
+app.config.from_pyfile('example_app.conf')
+
+# actually run the app if this is called as a script
+if __name__ == '__main__':
+    print('running')
+    app.run()
+
+def load_app_client():
+    return globus_sdk.ConfidentialAppAuthClient(
+        app.config['APP_CLIENT_ID'], app.config['APP_CLIENT_SECRET'])
+
+@app.route("/")
+def index():
+    """
+    This could be any page you like, rendered by Flask.
+    For this simple example, it will either redirect you to login, or print
+    a simple message.
+    """
+    if not session.get('is_authenticated'):
+        return redirect(url_for('login'))
+    return(str(session.get('username')) + ', you are logged in.')
+
+@app.route("/login")
+def login():
+    """
+    Login via Globus Auth.
+    May be invoked in one of two scenarios:
+
+      1. Login is starting, no state in Globus Auth yet
+      2. Returning to application during login, already have short-lived
+         code from Globus Auth to exchange for tokens, encoded in a query
+         param
+    """
+    # the redirect URI, as a complete URI (not relative path)
+    redirect_uri = url_for('login', _external=True)
+
+    auth_client = load_app_client()
+    auth_client.oauth2_start_flow(redirect_uri)
+
+    # If there's no "code" query string parameter, we're in this route
+    # starting a Globus Auth login flow.
+    # Redirect out to Globus Auth
+    if 'code' not in request.args:
+        auth_uri = auth_client.oauth2_get_authorize_url()
+        return redirect(auth_uri)
+    # If we do have a "code" param, we're coming back from Globus Auth
+    # and can start the process of exchanging an auth code for a token.
+    else:
+        code = request.args.get('code')
+        tokens_response = auth_client.oauth2_exchange_code_for_tokens(code)
+        ids = tokens_response.decode_id_token(auth_client)
+        session.update(
+                tokens=tokens_response.by_resource_server,
+                username=ids['preferred_username'],
+                is_authenticated=True
+                )
+        return redirect(url_for('index'))
+
+@app.route("/logout")
+def logout():
+    """
+    - Revoke the tokens with Globus Auth.
+    - Destroy the session state.
+    - Redirect the user to the Globus Auth logout page.
+    """
+    client = load_app_client()
+
+    # Revoke the tokens with Globus Auth
+    for token in (token_info['access_token']
+                  for token_info in session['tokens'].values()):
+        client.oauth2_revoke_token(token)
+
+    # Destroy the session state
+    session.clear()
+
+    # the return redirection location to give to Globus AUth
+    redirect_uri = url_for('index', _external=True)
+
+    # build the logout URI with query params
+    # there is no tool to help build this (yet!)
+    globus_logout_url = (
+        'https://auth.globus.org/v2/web/logout' +
+        '?client={}'.format(app.config['APP_CLIENT_ID']) +
+        '&redirect_uri={}'.format(redirect_uri) +
+        '&redirect_name=Globus Example App')
+
+    # Redirect the user to the Globus Auth logout page
+    return redirect(globus_logout_url)
+
---
+++ native_example.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import globus_sdk
+
+CLIENT_ID = '02a65a67-d996-4765-85b9-9522fb410d14'
+
+client = globus_sdk.NativeAppAuthClient(CLIENT_ID)
+client.oauth2_start_flow()
+
+authorize_url = client.oauth2_get_authorize_url()
+print('Please go to this URL and login: {0}'.format(authorize_url))
+
+# this is to work on Python2 and Python3 -- you can just use raw_input() or
+# input() for your specific version
+get_input = getattr(__builtins__, 'raw_input', input)
+auth_code = get_input(
+    'Please enter the code you get after login here: ').strip()
+token_response = client.oauth2_exchange_code_for_tokens(auth_code)
+
+globus_auth_data = token_response.by_resource_server['auth.globus.org']
+globus_transfer_data = token_response.by_resource_server['transfer.api.globus.org']
+
+# most specifically, you want these tokens as strings
+AUTH_TOKEN = globus_auth_data['access_token']
+TRANSFER_TOKEN = globus_transfer_data['access_token']
+
+# Let the user know that he/she is authenticated
+ids = token_response.decode_id_token(client)
+
+print("\n\nHere's what I know about you:\n\n")
+print(str(ids))
